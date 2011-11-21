a607dd74f4ec3f1b2ec6e781e706bb258adb176f
Michael Boratko <mboratko@starstreak.com>
2011-11-20 20:38:11 -0500

    Added jQuery (*sigh*, this is bigger than I wanted it to be) to make MathJax update on previews

--- lib/redmine_latex_mathjax/hooks/view_layouts_base_html_head_hook.rb
+++ lib/redmine_latex_mathjax/hooks/view_layouts_base_html_head_hook.rb
@@ -13,8 +13,43 @@ module RedmineLatexMathjax
     },
     'HTML-CSS': { availableFonts: ['TeX'] }
   });
+          MathJax.Hub.Typeset();
           </script>" +
-            javascript_include_tag('https://d3eoax9i5htok0.cloudfront.net/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML')
+            javascript_include_tag('https://d3eoax9i5htok0.cloudfront.net/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML') + 
+            javascript_include_tag('https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js')+
+            '<script>
+  jQuery.fn.contentChange = function(callback){
+    var elms = jQuery(this);
+    elms.each(
+      function(i){
+        var elm = jQuery(this);
+        elm.data("lastContents", elm.html());
+        window.watchContentChange = window.watchContentChange ? window.watchContentChange : [];
+        window.watchContentChange.push({"element": elm, "callback": callback});
+      }
+    )
+    return elms;
+  }
+  setInterval(function(){
+    if(window.watchContentChange){
+      for( i in window.watchContentChange){
+        try {
+        if(window.watchContentChange[i].element.data("lastContents") != window.watchContentChange[i].element.html()){
+          window.watchContentChange[i].callback.apply(window.watchContentChange[i].element);
+          window.watchContentChange[i].element.data("lastContents", window.watchContentChange[i].element.html())
+        }
+        }
+        catch(err){}
+      }
+    }
+  },500);
+</script>' + 
+            "<script>
+            $.noConflict();
+            jQuery(document).ready(function($) {
+                $('#preview').contentChange(function() { MathJax.Hub.Typeset(); } );
+            });
+            </script>" 
       end
     end
   end
