e4f3c79ed84a44e46cfe32956df39552da9c768c
René van Dorst <opensource@vdorst.com>
2015-08-07 12:11:42 +0200

    Enable Multiline MathJax syntax using blocktext input. Updated MathJax description.

--- init.rb
+++ init.rb
@@ -11,9 +11,12 @@ Redmine::Plugin.register :redmine_latex_mathjax_macro do
   version '0.3.0'
 
   Redmine::WikiFormatting::Macros.register do
-    desc = "MathJax Macro: Usage: {{mj( MathJax Syntax )}}"
-    macro :mj, :parse_args => false do |obj, args|
-      out = MathJaxEmbedMacro.mj_macro(obj, args)
+    desc "MathJax Macro:\n\n" +
+	    "Usage:\n"+ 
+	    "{{mj( single line MathJax Syntax )}}\n\n" +
+	    "{{mj\nMmulti line\nMathJax Syntax\n}}"
+    macro :mj, :parse_args => false do |obj, args, text|
+      out = MathJaxEmbedMacro.mj_macro(obj, args, text)
     end
   end
 end
@@ -23,13 +26,31 @@ class MathJaxEmbedMacro
   @@delimiter = '~~~'
   # Modify URL to MathJax.
   @@url = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js'
-  def self.mj_macro(obj, arg)
-    raise "Usage: {{mj( MathJax Syntax )}}" unless arg.length >= 1
-    if arg =~ /[<>]/
-	out = "<span id=\"errorExplanation\"><strong>MathJaxMacro:</strong> Illegal characters found! Don\'t using < or >.</span>".html_safe
-    else
-	out = "#@@delimiter #{arg} #@@delimiter".html_safe
+  def self.mj_error(text)
+	return "<span id=\"errorExplanation\"><strong>MathJaxMacro:</strong> #{text}</span>".html_safe
+  end
+  def self.mj_macro(obj, arg, text)
+    ## Check any argument is given
+    if ((text == nil) && (arg.length == 0))
+	return "<div id=\"errorExplanation\"><strong>MathJaxMacro:</strong> Usage:<ul><li><stong>Argument only</strong>: only single line MathJax Syntax</li>{{mj( MathJax Syntax )}}<li>Blocktext only: Multiline MathJax Syntax</li>{{mj<br/>Multi-<br/>line<br/>}}</ul></div>".html_safe
     end
+    ## Check if both argument are given
+    if ((text != nil) && (arg.length > 0))
+	return self.mj_error("Usage: Don\'t mix argument and blocktext!")
+    end
+    # Use text is arg == nil
+    if (text != nil)
+	arg = text
+    end
+    # Check argument length in Argument Mode
+    if (arg != nil) && (arg.length < 1)
+	return self.mj_error("Input must be atlease 1 characet long.")
+    end
+    # Check for illegal charaters.
+    if arg =~ /[<>]/
+	return self.mj_error("Illegal characters found! Don\'t using < or >")
+    end
+    return "#@@delimiter #{arg} #@@delimiter".html_safe
   end
   def self.delimiter()
 	  return @@delimiter
@@ -38,4 +59,3 @@ class MathJaxEmbedMacro
 	  return @@url
   end
 end
-
